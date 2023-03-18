222e98f191bc26d1a7fb6bd4e1c9cc6d8334cd46
bubbles sandcastle <bsandcastle@ironox.com>
2023-03-18 00:45:43 -0500

    some light refactoring on AppSheetView

--- iOS/AppSheetView.swift
+++ iOS/AppSheetView.swift
@@ -3,52 +3,40 @@ import SwiftUIKit
 import Combine
 
 
-struct AppSheetDetents {
-  static let min: CGFloat = 90
-  static let mid: CGFloat = 420
-  static let max: CGFloat = 750
-  
-  var min: CGFloat = 0
-  var mid: CGFloat = 0
-  var max: CGFloat = 0
-}
+let initialSheet = AppSheet.inbox
 
 
 struct AppSheetView: View {
   @ObservedObject var bundleCtrl = EmailBundleController.shared
   @ObservedObject var sheetCtrl = AppSheetController.shared
-  @State var selectedDetent: PresentationDetent = .height(1)
-  @State var config: AppSheet = .inbox
+  @State var selectedDetent: PresentationDetent = initialSheet.initialDetent
+  @State var config: AppSheet = initialSheet
   @State var detents: [UndimmedPresentationDetent] = [.height(AppSheetDetents.min)]
   
   // MARK: - VIEW
   
   var body: some View {
-    GeometryReader { geo in
-      ZStack(alignment: .top) {
-        OverlayBackgroundView()
-        Sheet
-          .interactiveDismissDisabled()
-          .presentationDetents(undimmed: detents, selection: $selectedDetent)
-          .presentationDragIndicator(.hidden)
-      }
-      .ignoresSafeArea()
-      .introspectViewController { $0.view.backgroundColor = .clear }
-      .animation(.spring(dampingFraction: 0.54), value: selectedDetent)
-      .onChange(of: geo.size) { _ in
-        sheetCtrl.sheetSize = geo.size
-      }
-      .onChange(of: bundleCtrl.selectedBundle) { _ in
-        selectedDetent = config.initialDetent
-      }
-      .onReceive(sheetCtrl.$sheet) { newConfig in
-        detents = newConfig.detents + config.detents
-        selectedDetent = newConfig.initialDetent
-        
-        Timer.after(0.1) { _ in
-          detents = newConfig.detents
-          withAnimation { config = newConfig }
-        }
+    ZStack(alignment: .top) {
+      OverlayBackgroundView()
+      Sheet
+        .interactiveDismissDisabled()
+        .presentationDetents(undimmed: detents, selection: $selectedDetent)
+        .presentationDragIndicator(.hidden)
+    }
+    .ignoresSafeArea()
+    .geo($sheetCtrl.sheetSize)
+    .introspectViewController { $0.view.backgroundColor = .clear }
+    .animation(.spring(dampingFraction: 0.54), value: selectedDetent)
+    .onChange(of: bundleCtrl.selectedBundle) { _ in
+      selectedDetent = config.initialDetent
+    }
+    .onReceive(sheetCtrl.$sheet) { newConfig in
+      detents = newConfig.detents + config.detents
+      selectedDetent = newConfig.initialDetent
+      
+      Timer.after(0.1) { _ in
+        detents = newConfig.detents
+        withAnimation { config = newConfig }
       }
     }
   }
@@ -83,20 +71,20 @@ struct AppSheet {
   static let inbox = Self(
     id: "inbox tools",
     detents: [
-      .height(90), // TODO: fix magic number
+      .height(AppSheetDetents.min),
       .medium,
       .large
     ],
-    initialDetent: .height(90)
+    initialDetent: .height(AppSheetDetents.min)
   )
   static let emailDetail = Self(
     id: "email tools",
     detents: [
-      .height(90), // TODO: fix magic number
+      .height(AppSheetDetents.min),
       .medium,
       .large
     ],
-    initialDetent: .height(90)
+    initialDetent: .height(AppSheetDetents.min)
   )
   static let createBundle = Self(
     id: "create bundle",
@@ -114,8 +102,20 @@ struct AppSheet {
   var initialDetent: PresentationDetent
 }
 
+
 extension AppSheet: Equatable {
   static func == (lhs: AppSheet, rhs: AppSheet) -> Bool {
     lhs.id == rhs.id
   }
+}
+
+
+struct AppSheetDetents {
+  static let min: CGFloat = 90
+  static let mid: CGFloat = 420
+  static let max: CGFloat = 750
+  
+  var min: CGFloat = 0
+  var mid: CGFloat = 0
+  var max: CGFloat = 0
 }
